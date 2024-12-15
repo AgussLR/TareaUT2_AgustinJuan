9ce2abf1f1543c8c7bd76310af2b537aab16f1ea
Juan Mateo <juanmateovargas23@gmail.com>
2024-12-15 21:41:11 +0100

    - Envio de email en la pantalla consultas, . Bases de datos en pantallas. perfil,mascotas y citas.

--- app/src/main/java/com/example/hakunamatata/perfil/PerfilData.kt
+++ app/src/main/java/com/example/hakunamatata/perfil/PerfilData.kt
@@ -1,8 +1,8 @@
 package com.example.hakunamatata.perfil
 
 data class PerfilData(
-    val nombre: String,
-    val apellidos: String,
-    val correo: String,
-    val telefono: String
+    val nombre: String? = null,
+    val apellidos: String? = null,
+    val correo: String? = null,
+    val telefono: String? = null
 )
